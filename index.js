6504a5cf379cbf92a3e83878fb97d691bbf2a908
Jack Yeung <jyeung.se@gmail.com>
2018-08-07 13:34:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function printBadges(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`)
+  }
+  return arr
+}
+
+
+function tailsNeverFails() {
+  let tailsCounter = 0
+  while (Math.random() >= 0.5) {
+    tailsCounter++
+  }
+  return `You got ${tailsCounter} tails in a row!`
+}
